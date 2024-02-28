9a14086c02da8e587741167e48153b2eaceebd29
DoloLeNom <doloadama@outlook.com>
2024-02-28 21:54:41 +0000

    .

---
+++ ALGO-PYTHON/Exercice4.py
@@ -0,0 +1,43 @@
+#Demander de donner le nombre de phrases
+while True:
+    N = input("Donner le nombre de numéros à saisir \n> ")
+    if N.isdigit() and int(N) > 0:
+        break
+    else:
+        print("Donnez un nombre correcte!!!!")
+N = int(N)
+
+#Initialisation
+commence = ["77","78","76","70","75"] #Contient les dépendances de début de numérp
+valide = "" #Récupère les numéros valide
+invalide = "" #Recupère les numéros invalides
+taille = 0 #Recupère la taille du numéro aprés retrait ou non des espaces
+
+#Saisie des numéros
+for i in range(N):
+    numb = input("Saisir un numéro: \n>")
+    debutNuméro = numb[:2]
+
+    #Vérifier les deux premiers digits
+    if debutNuméro in commence:
+        valide += numb
+        print("Numéro correct")
+    else:
+        invalide += numb
+        print("Numéro incorrect")
+
+    #Verifier la longueur du numéro
+    for j in range(len(numb)):
+            #Pour chaque espace 
+            if " " in numb:
+                taille = len(numb) - 1
+                continue
+    #veirfier si la taille après retrait des espaces est de 9
+    if taille == 9:
+        valide += numb
+        print(f"{valide:10}", end = "\t")
+        print()
+    else:
+        invalide += numb
+        print(f"{invalide:10}", end = "\t")
+        print()
