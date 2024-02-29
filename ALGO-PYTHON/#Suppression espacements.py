53dd685737477836fa57260643e32b5439735a24
DoloLeNom <doloadama@outlook.com>
2024-02-29 17:15:29 +0000

    .

--- ALGO-PYTHON/#Suppression espacements.py
+++
@@ -1,9 +0,0 @@
-    #Suppression espacements
-k=False
-for i in phrases:
-            if i == " " and  k:
-                continue
-            else:
-                phraseN += i
-                k = (i == " ") #mets à jour k en vérifiant si i == " " et met true
-print(phraseN, end="\n")
