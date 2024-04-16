bbc45715aabdce5be36a985d0631c904c564067e
doloadama <doloadama@outlook.com>
2024-04-16 17:51:51 +0000

    Deploy with streamlite

---
+++ App.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import csv
+import json
+import yaml
+from xml.etree import ElementTree as ET
+
+st.title("Convertisseur")
+
+st.write("Sur ce site vous pourrez convertir vos fichiers :blue[(CSV, XML, JSON, YAML)] \n "
+            "Comme vous le voudrez et cela gratuitement \n"
+            "Enjoy!!! :+1:")
+
+
+
